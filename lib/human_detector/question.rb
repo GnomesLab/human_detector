6fafb50252191ba98e0fabdc0d64b9b8415f74c8
João Correlo <joao.correlo@gnomeslab.com>
2010-11-11 19:45:52 +0000

    Question   - added attributes and validators

--- lib/human_detector/question.rb
+++ lib/human_detector/question.rb
@@ -1,2 +1,7 @@
 class Question < ActiveRecord::Base
+  atrr_acessible :question, :answer
+  
+  validates :question, :presence => true, :length => { :within => 3..30 }
+  validates :answer, :presence => true
+  
 end

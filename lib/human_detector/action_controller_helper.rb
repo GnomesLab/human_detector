dab9c7fe65c812a1f45bf8e5dbe71e4c7df27627
pdcoutinho <pdcoutinho@gmail.com>
2010-11-16 21:04:48 +0000

    remake on action_controller_helper

--- lib/human_detector/action_controller_helper.rb
+++ lib/human_detector/action_controller_helper.rb
@@ -2,39 +2,45 @@ module HumanDetector
 
   module ActionControllerHelper
 
+    # recebe do controller :only, :instance_variable, :flash => 'a' flash => {:error => 'x'}, :model, :callback
+
     def human_detection(options={})
-      before_filter :only => options[:only], :except => options[:except] do
-        human_filter(options.except(:only, :except))
+      before_filter :only => (options[:only].is_a?(Hash) ? options[:only].keys : options[:only]) do
+        human_filter(options)
       end
-    end
+    end # human_detection
 
     private
       def human_filter(options)
-        options.reverse_merge! :input_name => 'question_answer', :flash_error => 'Invalid captcha'
+        options.reverse_merge! :input_name => 'question_answer'
 
-        if params.include?('question_id') && params.include?(options[:input_name])
-          unless params[options[:input_name]] == Question.find_by_id(params['question_id']).try(:answer)
-            if options.has_key?(:callback) && self.responds_to?(options[:callback])
-              self.send(options[:callback])
-            else
-              if options.include?(:instance_variable) && options.include?(:model)
-                self.instance_variable_set(options[:instance_variable], options[:model])
-              else
-                self.instance_variable_set("@#{self.controller_name.singularize}".to_sym, self.controller_name.singularize.capitalize.constantize.new(params[self.controller_name.singularize.to_sym]))
-              end
-              flash[:error] = options[:flash_error]
-              case request.parameters[:action]
-              when 'create'
-                render :new
-              when 'update'
-                render :edit
-              end
-            end
-          end
-        end
+        human_fail(options) unless params.include?('question_id') && params[options[:input_name]] ==
+          Question.find_by_id(HumanDetector::Cipher.decrypt(params['question_id'])).try(:answer)
 
-      end
+      end # human_filter
 
-  end # ActionControllerHelper
+      def human_fail(options)
+        options.reverse_merge! :flash => { :error => 'Invalid captcha' }
+        return self.send(options[:callback]) if options.has_key?(:callback) && self.responds_to?(options[:callback])
 
-end # HumanDetector
+        options[:flash] = { :error => options[:flash] } if options[:flash].is_a?(String)
+        flash.merge! options[:flash]
+
+        model = options[:model] || self.controller_name.singularize.capitalize
+        instance_variable = options[:instance_variable] || "@#{model.downcase}".to_sym
+        
+        
+        
+        
+        
+
+      end # ActionControllerHelper
+
+  end # HumanDetector
+  
+  
+  # if options.include?(:instance_variable) && options.include?(:model)
+  #   self.instance_variable_set(options[:instance_variable], const_get(options[:model].to_s).new)
+  # else
+  #   self.instance_variable_set("@#{self.controller_name.singularize}".to_sym, self.controller_name.singularize.capitalize.constantize.new(params[self.controller_name.singularize.to_sym]))
+  # end

1acd98156f1c6755cdc298bc83eedf4ede743dbd
DBA <diogo.almeida@gnomeslab.com>
2010-11-16 11:36:15 +0000

    White spacing   - quick fix

--- lib/human_detector/question.rb
+++ lib/human_detector/question.rb
@@ -1,11 +1,11 @@
 module HumanDetector
 
+  # TODO: Add docs
   class Question < ActiveRecord::Base
     set_table_name 'human_detector_questions'
 
     validates :title, :presence => true
     validates :answer, :presence => true
+  end # Question
 
-  end
-
-end
+end # HumanDetector

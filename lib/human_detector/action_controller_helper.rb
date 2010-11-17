2b2d85601f2ac6400d32746dea424ce474540f44
pdcoutinho <pdcoutinho@gmail.com>
2010-11-17 16:49:50 +0000

    ActionControllerHelper  - added render  - some refactoring

--- lib/human_detector/action_controller_helper.rb
+++ lib/human_detector/action_controller_helper.rb
@@ -2,45 +2,47 @@ module HumanDetector
 
   module ActionControllerHelper
 
-    # recebe do controller :only, :instance_variable, :flash => 'a' flash => {:error => 'x'}, :model, :callback
-
     def human_detection(options={})
+      options.reverse_merge! @human_detection_defaults
       before_filter :only => (options[:only].is_a?(Hash) ? options[:only].keys : options[:only]) do
         human_filter(options)
       end
     end # human_detection
 
     private
-      def human_filter(options)
-        options.reverse_merge! :input_name => 'question_answer'
+      @human_detection_defaults = { :only => {:create => :new, :update => :edit},
+                                    :flash => { :error => 'Invalid captcha answer' },
+                                    :input_name => 'question_answer' }
 
+      def human_filter(options)
         human_fail(options) unless params.include?('question_id') && params[options[:input_name]] ==
           Question.find_by_id(HumanDetector::Cipher.decrypt(params['question_id'])).try(:answer)
-
       end # human_filter
 
       def human_fail(options)
-        options.reverse_merge! :flash => { :error => 'Invalid captcha' }
         return self.send(options[:callback]) if options.has_key?(:callback) && self.responds_to?(options[:callback])
 
+        set_flash_message(options)
+        instance_variable_creator(options)
+        render extract_render_name(options)
+      end # human_fail
+
+      def set_flash_message(options)
         options[:flash] = { :error => options[:flash] } if options[:flash].is_a?(String)
         flash.merge! options[:flash]
+      end # set_flash_message
 
-        model = options[:model] || self.controller_name.singularize.capitalize
+      def instance_variable_creator(options)
+        model = options[:model].to_s || self.controller_name.singularize.capitalize
         instance_variable = options[:instance_variable] || "@#{model.downcase}".to_sym
-        
-        
-        
-        
-        
+        self.instance_variable_set(instance_variable, model.contantize.new(params[model.downcase.to_sym]))
+      end # set_variable
 
-      end # ActionControllerHelper
+      def extract_render_name(options)
+        options[:only].is_a?(Hash) ?
+          options[:only][parameters[:action]] : @human_detection_defaults[:only][parameters[:action]] || :Default
+      end # extract_render_name
 
-  end # HumanDetector
-  
-  
-  # if options.include?(:instance_variable) && options.include?(:model)
-  #   self.instance_variable_set(options[:instance_variable], const_get(options[:model].to_s).new)
-  # else
-  #   self.instance_variable_set("@#{self.controller_name.singularize}".to_sym, self.controller_name.singularize.capitalize.constantize.new(params[self.controller_name.singularize.to_sym]))
-  # end
+  end # ActionControllerHelper
+
+end # HumanDetector
